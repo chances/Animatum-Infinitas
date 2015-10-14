bb5b62cdeac6aeaf07496dc1f4ebbfa4376ed077
Chance Snow <git@chancesnow.me>
2015-10-14 04:14:17 -0700

    Add resize behavior to webGL view

--- ts/controls/WebGLView.ts
+++ ts/controls/WebGLView.ts
@@ -3,15 +3,71 @@ import Component = require('../components/Component');
 class WebGLView extends Component {
     private renderer: THREE.WebGLRenderer = null;
     private scene: THREE.Scene = null;
-    private camera: THREE.Camera = null;
+    private camera: THREE.PerspectiveCamera = null;
 
     constructor () {
-        super('glView');
+        super('#glView');
 
-        $(() => {
-            this.ready();
+        this.ready();
+
+        this.optimizedResize.add(() => {
+            let width = this.e.width(),
+                height = this.e.height();
+
+            this.camera.aspect = width / height;
+            this.camera.updateProjectionMatrix();
+            this.renderer.setSize(width, height, true);
         });
     }
+
+    private optimizedResize = (function() {
+        var callbacks = [],
+            running = false;
+
+        // fired on resize event
+        function resize() {
+
+            if (!running) {
+                running = true;
+
+                if (window.requestAnimationFrame) {
+                    window.requestAnimationFrame(runCallbacks);
+                } else {
+                    setTimeout(runCallbacks, 66);
+                }
+            }
+
+        }
+
+        // run the actual callbacks
+        function runCallbacks() {
+
+            callbacks.forEach(function(callback) {
+                callback();
+            });
+
+            running = false;
+        }
+
+        // adds callback to loop
+        function addCallback(callback) {
+
+            if (callback) {
+                callbacks.push(callback);
+            }
+
+        }
+
+        return {
+            // public method to add additional callback
+            add: function(callback) {
+                if (!callbacks.length) {
+                    window.addEventListener('resize', resize);
+                }
+                addCallback(callback);
+            }
+        }
+    }());
 
     private ready() {
         let width = this.e.width(),
