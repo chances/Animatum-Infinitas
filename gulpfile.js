3e81750f89c96691d9e54a0c5c699454ced9152c
Chance Snow <git@chancesnow.me>
2015-10-16 12:03:07 -0700

    Fix browserify Three.js inclusion and add watchify

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var sourcemaps = require('gulp-sourcemaps');
 var compass = require('gulp-for-compass');
 var uglify = require('gulp-uglify');
 
+var watchify = require('watchify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
@@ -41,13 +42,20 @@ gulp.task('typescript', function (callback) {
     });
 });
 
-gulp.task('js', function () {
-    var b = browserify({
-        entries: './js/client/main.js',
-        debug: true
-    }).ignore('three');
+var bWatchify = watchify(browserify({
+    entries: './js/client/main.js',
+    debug: true
+}));
 
-    return b.bundle()
+// Add transformations here
+//bWatchify.transform(browserify-shim());
+
+bWatchify.on('update', bundle); // on any dep update, runs the bundler
+bWatchify.on('log', gutil.log); // output build logs to terminal
+
+function bundle() {
+    return bWatchify.bundle()
+        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
         .pipe(source('main.js'))
         .pipe(buffer())
         .pipe(sourcemaps.init({loadMaps: true}))
@@ -56,10 +64,34 @@ gulp.task('js', function () {
         //.pipe(rename({
         //    basename: 'main'
         //}))
-        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./dist/js/'));
- });
+}
+
+gulp.task('watch-js', bundle);
+
+gulp.task('js', ['typescript'], function () {
+    var b = browserify({
+        entries: './js/client/main.js',
+        debug: true
+    });
+
+    // Add transformations here
+    //b.transform(browserify-shim());
+
+    return b.bundle()
+        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+        .pipe(source('main.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        //.pipe(rename({
+        //    basename: 'main'
+        //}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./dist/js/'));
+});
 
 gulp.task('watch', ['typescript'], function () {
     var watcher = gulp.watch(tsSource, ['typescript']);
