14f9fc6e7fa71ef1554fa1899b19a02d8e033dba
Chance Snow <git@chancesnow.me>
2015-10-16 18:48:04 -0700

    Refactor for naming conflict and removal of services global

--- ts/main.ts
+++ ts/main.ts
@@ -1,18 +1,6 @@
 import Events = require('./Bridge');
 import Application = require('./Application');
 
-var services: {
-    events: Events.Bridge;
-} = {
-    events: new Events.Bridge()
-};
+var animatumApp = new Application();
 
-//$.fn.hasAttr = function(name: string): boolean {
-//    var attr = this.attr(name);
-//    return attr !== undefined && attr !== false;
-//    //return this.attr(name) !== undefined;
-//};
-
-var app = new Application();
-
-app.debug();
+animatumApp.debug();
